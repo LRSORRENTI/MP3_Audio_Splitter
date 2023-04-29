9a1dc59ecb3174c03249f7eafdf70c51fcc4e5e4
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-04-29 11:02:56 -0500

    added more comments, the fs.readfile method takes two args, the path to mp3 file, second arg is callback function with two args, err and data, err will log an error obj if error occurs, second arg contains contents of file

--- src/splitMP3.ts
+++ src/splitMP3.ts
@@ -55,3 +55,79 @@ ffmpeg.setFfmpegPath(ffmpeg_static);
 //  Finally, this last line: the setFfmpegPath
 //    method sets the path to the 
 //ffmpeg binary that we just installed.
+
+
+/*
+This line of below: fs.readFile('mp3Files\mp3FileIOwn.mp3', (err, data) => {
+
+code is using the Node.js fs module
+ to read the contents of the file mp3FileIOwn.mp3 
+ located in the mp3Files directory. 
+ 
+ The readFile method takes two arguments:
+
+ The first argument is the path
+  to the file that you want to read.
+   In this case, the path is 
+   'mp3Files\mp3FileIOwn.mp3'.
+
+The second argument is a callback 
+function that will be executed once 
+the file has been read. This callback
+ function takes two parameters:
+ 
+ err and data. 
+ 
+ If there is an error while
+  reading the file, err will
+   contain an error object. 
+   If there is no error,
+    data will contain the 
+    contents of the file.
+
+So essentially, this line of 
+code is reading the contents 
+of the mp3FileIOwn.mp3 file located 
+in the mp3Files directory and 
+executing a callback function 
+once the file has been read:
+
+*/
+fs.readFile('mp3Files\mp3FileIOwn.mp3', (err, data) => {
+    // the '\P' text color change in VSCODE confused
+    // me so I asked ChatGPT: 
+// No, the color of the text in 
+// VSCode does not affect the functionality 
+// of the code. The color is just an 
+// indication to help make the code more
+//  readable for the programmer. 
+//  As long as the path string is 
+//  formatted correctly with the right
+//   characters and in the right order, it should
+//  work fine with the fs.readFile function.
+
+
+/* 
+
+The throw keyword below is used to throw 
+an error object and terminate
+ the program's execution when an error occurs.
+ 
+ If an error is thrown in the readFile 
+ callback, the program will stop 
+ executing and the error message will
+  be printed to the console. 
+  
+  If you don't throw an error in 
+  the callback, the program will 
+  continue to execute even if there
+   were errors during the file reading process:
+*/
+if (err) throw err;
+
+
+  
+    // Continue with splitting the file
+  });
+
+ 
